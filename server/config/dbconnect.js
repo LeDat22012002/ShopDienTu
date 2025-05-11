b3892524422a25d3887ab4e6e636fae066e8cb15
LeDat22012002 <ledat22.01.2002@gmail.com>
2025-05-11 20:59:00 +0700

    deloyDatabase

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -1,15 +1,15 @@
-const {default : mongoose} = require('mongoose')
+const { default: mongoose } = require('mongoose');
 
-const dbConnect = async() => {
+const dbConnect = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_ULR)
-        if(conn.connection.readyState ===1 ) {
-            console.log(' Connect Database thanh cong !')
-        }else console.log('Connect Database that bai !')
+        const conn = await mongoose.connect(process.env.MONGODB_ULR_ATLATS);
+        if (conn.connection.readyState === 1) {
+            console.log(' Connect Database thanh cong !');
+        } else console.log('Connect Database that bai !');
     } catch (error) {
-        console.log('co loi' )
-        throw new Error(error)
+        console.log('co loi');
+        throw new Error(error);
     }
-}
+};
 
-module.exports =dbConnect
+module.exports = dbConnect;
