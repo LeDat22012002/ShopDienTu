991a6b36533a6adf1d4f1b032ab6e4f9eb7bd2bd
LeDat22012002 <ledat22.01.2002@gmail.com>
2025-05-12 16:50:12 +0700

    FixLogin33

--- server/Passport.js
+++ server/Passport.js
@@ -25,7 +25,8 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/api/user/google/callback',
+            callbackURL:
+                'https://shopdientu-rg6y.onrender.com/api/user/google/callback',
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
