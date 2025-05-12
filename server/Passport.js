bb069242d281fa8ce27695f9d4af028bb57f12e2
LeDat22012002 <ledat22.01.2002@gmail.com>
2025-05-12 16:31:51 +0700

    FixLogin

--- server/Passport.js
+++ server/Passport.js
@@ -17,6 +17,8 @@ passport.deserializeUser(async (id, done) => {
     const user = await User.findById(id);
     done(null, user);
 });
+console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
+console.log('GOOGLE_CLIENT_SECRET:', process.env.GOOGLE_CLIENT_SECRET);
 
 passport.use(
     new GoogleStrategy(
