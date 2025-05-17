bfac33788766f0b3e148b8f8815d7429f07cd106
LeDat22012002 <ledat22.01.2002@gmail.com>
2025-05-17 09:39:39 +0700

    Reponsive7

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require('mongoose');
 
 const dbConnect = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_ULR);
+        const conn = await mongoose.connect(process.env.MONGODB_ULR_ATLATS);
         if (conn.connection.readyState === 1) {
             console.log(' Connect Database thanh cong !');
         } else console.log('Connect Database that bai !');
